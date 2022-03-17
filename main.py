592adb891d6c4bdafabd94ebceb54a94c87cbe49
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-03-17 11:35:49 +0100

    cas

--- main.py
+++ main.py
@@ -4,30 +4,35 @@ start = False
 začátek = 1
 konec = 2
 reset = 0
-cas = control.millis()
+cas_start = control.millis()
+cas_konec = control.millis()
 RunComp.set_light_level()
 def on_light_drop(): 
+    global cas_start, cas_konec
     if not start:
         radio.send_number(1)
-        cas
+        cas_start
         start = True
-        console.log_value("x", cas)
     if start:
         radio.send_number(2)
-        cas
+        cas_konec
         start = False
 RunComp.on_light_drop(on_light_drop)
 
 def on_received_number(receivedNumber):
+    global cas_konec, cas_start
     if receivedNumber == 1:
-        cas
+        cas_start
         start = True
     if receivedNumber == 2:
-        cas
+        cas_konec
         start = False
+    if receivedNumber == 0:
+        control.reset()
 radio.on_received_number(on_received_number)
 def on_button_pressed_a():
     control.reset()
     radio.send_number(0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
-print (cas)
+cas = cas_konec - cas_start
+print(cas)
--- main.ts
+++ main.ts
@@ -4,34 +4,40 @@ let start = false
 let začátek = 1
 let konec = 2
 let reset = 0
-let cas = control.millis()
+let cas_start = control.millis()
+let cas_konec = control.millis()
 RunComp.SetLightLevel()
 RunComp.OnLightDrop(function on_light_drop() {
     let start: boolean;
+    
     if (!start) {
         radio.sendNumber(1)
-        cas
+        cas_start
         start = true
-        console.logValue("x", cas)
     }
     
     if (start) {
         radio.sendNumber(2)
-        cas
+        cas_konec
         start = false
     }
     
 })
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     let start: boolean;
+    
     if (receivedNumber == 1) {
-        cas
+        cas_start
         start = true
     }
     
     if (receivedNumber == 2) {
-        cas
+        cas_konec
         start = false
+    }
+    
+    if (receivedNumber == 0) {
+        control.reset()
     }
     
 })
@@ -39,4 +45,5 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     control.reset()
     radio.sendNumber(0)
 })
+let cas = cas_konec - cas_start
 console.log(cas)
