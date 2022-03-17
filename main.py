2f71c9f9e23fd615c946097f4d166e55ea6b55c2
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-03-17 11:24:25 +0100

    přidání

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 radio.set_group(9)
 basic.clear_screen()
+start = False
+začátek = 1
+konec = 2
+reset = 0
+cas = control.millis()
 RunComp.set_light_level()
 def on_light_drop(): 
-    radio.send_number(1)
+    if not start:
+        radio.send_number(1)
+        cas
+        start = True
+        console.log_value("x", cas)
+    if start:
+        radio.send_number(2)
+        cas
+        start = False
 RunComp.on_light_drop(on_light_drop)
+
+def on_received_number(receivedNumber):
+    if receivedNumber == 1:
+        cas
+        start = True
+    if receivedNumber == 2:
+        cas
+        start = False
+radio.on_received_number(on_received_number)
+def on_button_pressed_a():
+    control.reset()
+    radio.send_number(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+print (cas)
--- main.ts
+++ main.ts
@@ -1,6 +1,42 @@
 radio.setGroup(9)
 basic.clearScreen()
+let start = false
+let začátek = 1
+let konec = 2
+let reset = 0
+let cas = control.millis()
 RunComp.SetLightLevel()
 RunComp.OnLightDrop(function on_light_drop() {
-    radio.sendNumber(1)
+    let start: boolean;
+    if (!start) {
+        radio.sendNumber(1)
+        cas
+        start = true
+        console.logValue("x", cas)
+    }
+    
+    if (start) {
+        radio.sendNumber(2)
+        cas
+        start = false
+    }
+    
 })
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    let start: boolean;
+    if (receivedNumber == 1) {
+        cas
+        start = true
+    }
+    
+    if (receivedNumber == 2) {
+        cas
+        start = false
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    control.reset()
+    radio.sendNumber(0)
+})
+console.log(cas)
